711ee30bce07eb99383b65946d9deadfcc71ea70
Andres Gonzalez Gongora <30781086+Zappandy@users.noreply.github.com>
2020-07-17 10:10:46 -0500

    Create Batch_1Sheets.py

---
+++ Batch_1Sheets.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import re
+
+
+headers = {"Connection on LinkedIn": [], "First Name": [], "Last Name": [], "Title": [],
+           "Email": [], "Phone": [], "Company Name": [], "Company Website": [],
+           "Street Address": [], "Country": [], "City": [], "State": [], "Zip": [], "Phones 1": [],
+           "Notes": []}
+
+master_df = pd.DataFrame(headers)
+
+
+def Wuxi_sheet(df):
+    """
+    pandas dataframe --> pandas dataframe
+    Wuxi_sheet(df) --> dataframe has now naming data, email et al.
+    :param df: Master Farmacon dataframe
+    :return: Updated data frame
+    """
+    df_copy = df.copy()
+    fileRegex = re.compile(r"^(Farmacon)(.*)(Wuxi.csv)$")
+    for file in os.listdir():
+        if fileRegex.search(file):
+            sheet = pd.read_csv(file).copy()
+            break
+    list(sheet.keys())  # ['First Name', 'Last Name', 'Title', 'Company']
+    for head in sheet.head(0):
+        if head in df_copy.head(0): #and df_copy[head].empty:
+            pd.merge(df_copy, sheet, on=head)
+            #df_copy[head] = sheet[head]
+        else:
+            df_copy["Company Name"] = sheet[head]
+    return df_copy
