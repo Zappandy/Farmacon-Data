c54ac5d5db05af45b588838dc1de1b1caa8ba866
Andres Gonzalez Gongora <30781086+Zappandy@users.noreply.github.com>
2020-07-17 10:33:52 -0500

    Update Batch_1Sheets.py

--- Batch_1Sheets.py
+++ Batch_1Sheets.py
@@ -31,3 +31,25 @@ def Wuxi_sheet(df):
         else:
             df_copy["Company Name"] = sheet[head]
     return df_copy
+
+
+def MeridianCTSpeakers_Sheet(df):
+    """
+    pandas dataframe --> pandas dataframe
+    Wuxi_sheet(df) --> dataframe has now naming data, email et al.
+    :param df: Master Farmacon dataframe
+    :return: Updated data frame
+    """
+    df_copy = df.copy()
+    fileRegex = re.compile(r"^(Farmacon)(.*)(Meridian CT Speakers.csv)$")
+    for file in os.listdir():
+        if fileRegex.search(file):
+            sheet = pd.read_csv(file).copy()
+            break
+    list(sheet.keys())  # ['First Name', 'Last Name', 'Title', 'Company']
+    for head in sheet.head(0):
+        if head in df_copy.head(0): #and df_copy[head].empty:
+            df_copy[head] = sheet[head]
+        else:
+            df_copy["Company Name"] = sheet[head]
+    return df_copy
